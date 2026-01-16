09b31c8e3b8d4b42340dc8e63974f63914ba00e8
vanvakhtl <ivan.v@tlight.biz>
2026-01-16 08:26:22 +0300

    fix: fix database reconnect issue

--- huey_django_orm/storage.py
+++ huey_django_orm/storage.py
@@ -1,11 +1,12 @@
 import logging
 import threading
 
+from decorators import retry_on_db_disconnect
 from django.apps import apps as django_apps
 from django.utils.timezone import make_aware
 from huey.api import Huey
 from huey.constants import EmptyData
-from huey.storage import BaseStorage, to_bytes, to_blob
+from huey.storage import BaseStorage, to_blob, to_bytes
 
 logger = logging.getLogger(__name__)
 
@@ -38,6 +39,7 @@ class DjangoORMStorage(BaseStorage):
             self.keystore_model = django_apps.get_model("huey_django_orm.HueyKv", require_ready=False)
         return self.keystore_model.objects.filter(queue=self.name)
 
+    @retry_on_db_disconnect()
     def dequeue(self):
         with self.dequeue_lock:
             try:
@@ -50,14 +52,17 @@ class DjangoORMStorage(BaseStorage):
                     result.delete()
                     return to_bytes(data)
 
+    @retry_on_db_disconnect()
     def enqueue(self, data, priority=None):
         if self.task_model is None:
             self.task_model = django_apps.get_model("huey_django_orm.HueyTask", require_ready=False)
         self.task_model.objects.create(queue=self.name, data=data, priority=priority)
-
+    
+    @retry_on_db_disconnect()
     def queue_size(self):
         return self.queue_items.count()
 
+    @retry_on_db_disconnect()
     def enqueued_items(self, limit=None):
         items = self.queue_items
         if isinstance(limit, int):
@@ -65,14 +70,17 @@ class DjangoORMStorage(BaseStorage):
 
         return [to_bytes(i.data) for i in items]
 
+    @retry_on_db_disconnect()
     def flush_queue(self):
         self.queue_items.delete()
 
+    @retry_on_db_disconnect()
     def add_to_schedule(self, data, ts, utc=None):
         if self.schedule_model is None:
             self.schedule_model = django_apps.get_model("huey_django_orm.HueySchedule", require_ready=False)
         self.schedule_model.objects.create(queue=self.name, data=to_blob(data), timestamp=make_aware(ts))
 
+    @retry_on_db_disconnect()
     def read_schedule(self, ts):
         scheduled_tasks = self.schedule_tasks.filter(timestamp__lte=make_aware(ts))
 
@@ -81,9 +89,11 @@ class DjangoORMStorage(BaseStorage):
 
         return data
 
+    @retry_on_db_disconnect()
     def schedule_size(self):
         return self.schedule_tasks.count()
 
+    @retry_on_db_disconnect()
     def scheduled_items(self, limit=None):
         scheduled_tasks = self.schedule_tasks
 
@@ -92,14 +102,17 @@ class DjangoORMStorage(BaseStorage):
 
         return [to_bytes(i.data) for i in scheduled_tasks]
 
+    @retry_on_db_disconnect()
     def flush_schedule(self):
         self.schedule_tasks.delete()
 
+    @retry_on_db_disconnect()
     def put_data(self, key, value, is_result=False):
         if self.keystore_model is None:
             self.keystore_model = django_apps.get_model("huey_django_orm.HueyKv", require_ready=False)
         self.keystore_model.objects.create(queue=self.name, key=key, value=to_blob(value))
 
+    @retry_on_db_disconnect()
     def peek_data(self, key):
         try:
             res = self.values.get(key=key)
@@ -109,6 +122,7 @@ class DjangoORMStorage(BaseStorage):
             if res is not None:
                 return to_bytes(res.value)
 
+    @retry_on_db_disconnect()
     def pop_data(self, key):
         try:
             res = self.values.get(key=key)
@@ -121,21 +135,26 @@ class DjangoORMStorage(BaseStorage):
 
                 return data
 
+    @retry_on_db_disconnect()
     def has_data_for_key(self, key):
         return self.peek_data(key) != EmptyData
 
+    @retry_on_db_disconnect()
     def put_if_empty(self, key, value):
         if not self.has_data_for_key(key):
             self.put_data(key, value)
             return True
         return False
 
+    @retry_on_db_disconnect()
     def result_store_size(self):
         return self.values.count()
 
+    @retry_on_db_disconnect()
     def result_items(self):
         return dict((i.key, to_bytes(i.value)) for i in self.values)
 
+    @retry_on_db_disconnect()
     def flush_results(self):
         self.values.delete()
 
